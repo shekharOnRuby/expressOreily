ef4576bd09345a08927e929d693beeedde97f212
shekhar karande <shekhar.karande@gmail.com>
2017-07-30 03:43:54 +0100

    adding bits about bootstrap

---
+++ 3.4/index.js
@@ -0,0 +1,51 @@
+var express = require('express');
+var app = express();
+var url = require('url');
+var morgan = require('morgan');
+
+app.use(morgan('short'));
+
+app.set('views', './views');
+app.set('view engine','ejs');
+
+app.get('/', function(req,res){
+	res.render('pages/four',{
+		
+	});
+});
+
+var memberAPI = express.Router();
+
+memberAPI.get('/', function(req,res){
+	res.send('Member Home Page');
+});
+
+memberAPI.get('/profile',function(req, res){
+	res.send('Member Profile page');
+
+});
+
+memberAPI.get('/changePassword',function(req, res){
+	res.send('Member Password change request');
+
+});
+
+var adminAPI = express.Router();
+
+adminAPI.get('/',function(req, res){
+	res.send('Admin Home page');
+});
+
+adminAPI.get('/',function(req, res){
+	res.send('Admin Home page');
+});
+
+app.use('/api/member',memberAPI);
+app.use('/api/admin',adminAPI);
+app.use(express.static(__dirname + '/public'));
+
+app.listen(8080,function(){
+	console.log('Server listening on port 8080');
+});
+
+
